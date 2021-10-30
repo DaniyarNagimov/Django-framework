59fe690f3dd0137e216012f34be4dd313c163fcf
Daniyar <daniyarsamurai@gmail.com>
2021-10-30 21:51:44 +0300

    Lesson 6

--- geekshop/mainapp/admin.py
+++ geekshop/mainapp/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 
-<<<<<<< HEAD
 from mainapp.models import Product, ProductCategory
 
 admin.site.register(Product)
 admin.site.register(ProductCategory)
-=======
-# Register your models here.
->>>>>>> 90ae9ea75642a8fad139b0e4e04c3ac457a35197
